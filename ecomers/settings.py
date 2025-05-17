ca4029d880f48b3d678486be0cc7312b7c7be59e
satyanarayana902 <balu.medicherla959@gmail.com>
2025-05-17 22:31:57 +0530

    Update settings.py

--- ecomers/settings.py
+++ ecomers/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-_+pbqc*-1@%2!8%jdjg*++=&=qz9jpczly=n#ju&q!py*n!2x-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
